26b59682000a1b7edda4f91eae7cabf78c675d97
bengiv <67504107+bengiv@users.noreply.github.com>
2023-08-04 13:31:45 +0200

    initial

---
+++ src/main/java/me/bengiv/orb/OresAreBack.java
@@ -0,0 +1,20 @@
+package me.bengiv.orb;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OresAreBack implements ModInitializer {
+	public static final String MOD_ID = "orb";
+    public static final Logger LOGGER = LoggerFactory.getLogger("orb");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("init");
+	}
+}
