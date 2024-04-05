dc4d617e7afbe5515e562808d8957e8399324476
Ben <67504107+bengiv@users.noreply.github.com>
2024-04-05 19:01:13 +0100

    orbing the orb

--- src/main/java/dev/bengiv/Orb.java
+++ src/main/java/dev/bengiv/Orb.java
@@ -18,6 +18,6 @@ public class Orb implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Initialized Mod");
+		LOGGER.info("[ORB] Initialized Mod");
 	}
 }
